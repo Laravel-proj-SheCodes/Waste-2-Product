c3451b35b49a8f56d21d6b634ba257f87732cf57
FouzriChayma <chayma.fouzri@esprit.tn>
2025-09-25 11:09:18 +0100

    integration back office Dashboard

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -10,68 +10,65 @@
     <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/dashboard') }}">
-                    <i class="material-symbols-rounded opacity-5">dashboard</i>
-                    <span class="nav-link-text ms-1">Dashboard</span>
+                <a class="nav-link text-dark" href="{{ url('/users') }}">
+                    <i class="material-symbols-rounded opacity-5">people</i>
+                    <span class="nav-link-text ms-1">Users</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/tables') }}">
-                    <i class="material-symbols-rounded opacity-5">table_view</i>
-                    <span class="nav-link-text ms-1">Tables</span>
+                <a class="nav-link text-dark" href="{{ url('/profiles') }}">
+                    <i class="material-symbols-rounded opacity-5">account_circle</i>
+                    <span class="nav-link-text ms-1">Profiles</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/billing') }}">
-                    <i class="material-symbols-rounded opacity-5">receipt_long</i>
-                    <span class="nav-link-text ms-1">Billing</span>
+                <a class="nav-link text-dark" href="{{ url('/waste-posts') }}">
+                    <i class="material-symbols-rounded opacity-5">delete</i>
+                    <span class="nav-link-text ms-1">Waste Posts</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/virtual-reality') }}">
-                    <i class="material-symbols-rounded opacity-5">view_in_ar</i>
-                    <span class="nav-link-text ms-1">Virtual Reality</span>
+                <a class="nav-link text-dark" href="{{ url('/proposals') }}">
+                    <i class="material-symbols-rounded opacity-5">edit_note</i>
+                    <span class="nav-link-text ms-1">Proposals</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/rtl') }}">
-                    <i class="material-symbols-rounded opacity-5">format_textdirection_r_to_l</i>
-                    <span class="nav-link-text ms-1">RTL</span>
+                <a class="nav-link text-dark" href="{{ url('/offers') }}">
+                    <i class="material-symbols-rounded opacity-5">local_offer</i>
+                    <span class="nav-link-text ms-1">Offers</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/notifications') }}">
-                    <i class="material-symbols-rounded opacity-5">notifications</i>
-                    <span class="nav-link-text ms-1">Notifications</span>
-                </a>
-            </li>
-            <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Account pages</h6>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/profile') }}">
-                    <i class="material-symbols-rounded opacity-5">person</i>
-                    <span class="nav-link-text ms-1">Profile</span>
+                <a class="nav-link text-dark" href="{{ url('/transactions') }}">
+                    <i class="material-symbols-rounded opacity-5">swap_horiz</i>
+                    <span class="nav-link-text ms-1">Transactions</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/sign-in') }}">
-                    <i class="material-symbols-rounded opacity-5">login</i>
-                    <span class="nav-link-text ms-1">Sign In</span>
+                <a class="nav-link text-dark" href="{{ url('/commands') }}">
+                    <i class="material-symbols-rounded opacity-5">list_alt</i>
+                    <span class="nav-link-text ms-1">Commands</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ url('/sign-up') }}">
-                    <i class="material-symbols-rounded opacity-5">assignment</i>
-                    <span class="nav-link-text ms-1">Sign Up</span>
+                <a class="nav-link text-dark" href="{{ url('/payments') }}">
+                    <i class="material-symbols-rounded opacity-5">payment</i>
+                    <span class="nav-link-text ms-1">Payments</span>
                 </a>
             </li>
-        </ul>
-    </div>
-    <div class="sidenav-footer position-absolute w-100 bottom-0">
-        <div class="mx-3">
-            <a class="btn btn-outline-dark mt-4 w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/overview/material-dashboard?ref=sidebarfree" type="button">Documentation</a>
-            <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/material-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
-        </div>
-    </div>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="{{ url('/transformations') }}">
+                    <i class="material-symbols-rounded opacity-5">build</i>
+                    <span class="nav-link-text ms-1">Transformations</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="{{ url('/products') }}">
+                    <i class="material-symbols-rounded opacity-5">inventory</i>
+                    <span class="nav-link-text ms-1">Products</span>
+                </a>
+            </li>
+            
+
 </aside>
