714794e93ee1d6e35de43ad020bffebcaa338695
FouzriChayma <chayma.fouzri@esprit.tn>
2025-09-25 11:00:32 +0100

    first commit

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="container-fluid py-2">
+        <!-- Dashboard content goes here -->
+        <h1>Welcome to the Dashboard</h1>
+    </div>
+@endsection
