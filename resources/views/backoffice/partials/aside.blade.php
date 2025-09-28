a96f85427a40e09f9c539c910f109b6acfd7dfe9
mouna-mbr <mouna.benrebah@esprit.tn>
2025-09-28 19:23:35 +0100

    icon transaction in backoffice

--- resources/views/backoffice/partials/aside.blade.php
+++ resources/views/backoffice/partials/aside.blade.php
@@ -30,10 +30,11 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link text-dark" href="{{ url('/transactions-troc') }}">
-                    <i class="material-symbols-rounded opacity-5">swap_horiz</i>
-                    <span class="nav-link-text ms-2">transactions de Troc</span>
+                    <i class="material-symbols-rounded opacity-5">local_shipping</i>
+                    <span class="nav-link-text ms-2">Transactions de Troc</span>
                 </a>
             </li>
+
                 <li class="nav-item">
                 <a class="nav-link text-dark" href="{{ route('postdechets.index') }}">
                     <i class="material-symbols-rounded opacity-5">delete</i>
