bb41abb6eed2d45c0bc5dd78c800cccd3cf8a94f
tesnimSatouri <tesnim.satouri@esprit.tn>
2025-09-28 21:19:22 +0100

    Refactor marketplace navigation to use a dropdown menu; enhance delete confirmation and error handling in annonce deletion

--- resources/views/frontoffice/partials/navbarfront.blade.php
+++ resources/views/frontoffice/partials/navbarfront.blade.php
@@ -51,15 +51,34 @@
             </li>
           @endif
 
-          {{-- Marketplace --}}
-          @if (Route::has('marketplace'))
-            <li class="nav-item">
-              <a class="nav-link {{ request()->routeIs('marketplace') ? 'active' : '' }}"
-                 href="{{ route('marketplace') }}">
-                Marketplace
-              </a>
-            </li>
-          @endif
+        {{-- Marketplace Dropdown --}}
+@if (Route::has('marketplace') || Route::has('commandes.index'))
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle {{ request()->routeIs('marketplace') || request()->routeIs('commandes.*') ? 'active' : '' }}"
+           href="#" id="marketplaceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Marketplace
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="marketplaceDropdown">
+            @if (Route::has('marketplace'))
+                <li>
+                    <a class="dropdown-item {{ request()->routeIs('marketplace') ? 'active' : '' }}"
+                       href="{{ route('marketplace') }}">
+                        Marketplace Home
+                    </a>
+                </li>
+            @endif
+            @if (Route::has('commandes.index'))
+                <li>
+                    <a class="dropdown-item {{ request()->routeIs('commandes.*') ? 'active' : '' }}"
+                       href="{{ route('commandes.page') }}">
+                        My Orders
+                    </a>
+                </li>
+            @endif
+        </ul>
+    </li>
+@endif
+
 
           {{-- Troc (actif sur /home/troc et /home/offres-troc/*) --}}
           @php
